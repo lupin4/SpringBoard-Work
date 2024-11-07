907db92d19ef254e348b341718899b8393512f9d
lupin4 <davidclabaugh@fullsail.edu>
2024-11-07 07:25:54 -0800

    added sorting algorithms exercise

--- Intermediate_Javascript/Algorithms/dsa-sorting-exercise-starter-code/dsa-sorting/quick.js
+++ Intermediate_Javascript/Algorithms/dsa-sorting-exercise-starter-code/dsa-sorting/quick.js
@@ -3,12 +3,39 @@ pivot accepts an array, starting index, and ending index
 You can assume the pivot is always the first element
 */
 
-function pivot(){}
+function pivot(arr, start = 0, end = arr.length - 1) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  let pivotIndex = start;
+  let pivotValue = arr[start];
+
+  for (let i = start + 1; i <= end; i++) {
+    if (arr[i] < pivotValue) {
+      pivotIndex++;
+      swap(arr, pivotIndex, i);
+    }
+  }
+
+  swap(arr, start, pivotIndex);
+  return pivotIndex;
+}
 
 /*
 quickSort accepts an array, left index, and right index
 */
 
-function quickSort() {}
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
 
-module.exports = quickSort;
+module.exports = {
+  pivot,
+  quickSort,
+};
--- Intermediate_Javascript/Algorithms/dsa-sorting-exercise-starter-code/dsa-sorting/radix.js
+++ Intermediate_Javascript/Algorithms/dsa-sorting-exercise-starter-code/dsa-sorting/radix.js
@@ -1,3 +1,44 @@
-function radixSort() {}
+function radixSort(arr) {
+  const maxDigit = mostDigits(arr);
+  for (let k = 0; k < maxDigit; k++) {
+      let digitBuckets = Array.from({ length: 10 }, () => []);
+      
+      // Place each number in the corresponding bucket based on the current digit
+      for (let i = 0; i < arr.length; i++) {
+          let digit = getDigit(arr[i], k);
+          digitBuckets[digit].push(arr[i]);
+      }
 
-module.exports = radixSort;
+    // Flatten the buckets back into the array using the rest operator
+    arr = [].concat(...digitBuckets);
+  }
+  return arr;
+}
+
+// Helper function to get the digit at a specific place
+function getDigit(num, place) {
+  return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+}
+
+// Helper function to determine the number of digits in the largest number
+function mostDigits(nums) {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+      maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
+  return maxDigits;
+}
+
+// Helper function to count the number of digits in a number
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+// Export all functions that are being tested
+module.exports = {
+  radixSort,
+  getDigit,
+  digitCount,
+  mostDigits
+};
